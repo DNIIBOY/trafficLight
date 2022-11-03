57d2416b343a24b0b4acfa7d613c099ca00dfc89
DNIIBOY <daniel@nettelfield.com>
2022-11-03 22:34:01 +0100

    add project files

---
+++ trafficLight/trafficLight/main.c
@@ -0,0 +1,18 @@
+/*
+ * trafficLight.c
+ *
+ * Created: 03-11-2022 22:33:02
+ * Authors : Anders, Daniel & Oliver
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
