ef333c69ea3b5b180a1df7015db487e004aa1b6b
DNIIBOY <daniel@nettelfield.com>
2022-11-03 23:01:45 +0100

    remove ugly newlines

--- trafficLight/main.c
+++ trafficLight/main.c
@@ -7,18 +7,15 @@
 
 
 #define F_CPU 16000000UL
-
 #include <avr/io.h>
 #include <avr/power.h>
 #include <util/delay.h>
-
 #include <stdbool.h>  // Add boolean data type
 
 // Globals
 int greenTime = 10;  // How many seconds can the light stay green?
 int pedDelay = 2;  // How much earlier do the pedestrian lights turn off, than the car lights?
 int carDelay = greenTime * 0.1;  // How long are all traffic lights red, when changing from one side to another?
-
 bool currentlyNorthbound = false;  // Is the traffic currently northbound?
 
 
