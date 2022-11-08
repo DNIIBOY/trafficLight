06680d1f49477856edab17fd99d9500abeafc9c2
DNIIBOY <daniel@nettelfield.com>
2022-11-08 10:35:37 +0100

    create 2d array for picking traffic light, closes #6)

--- trafficLight/main.c
+++ trafficLight/main.c
@@ -19,6 +19,12 @@
 
 bool currentlyNorthbound = false;  // Is the traffic currently northbound?
 
+char trafficLights[2][4] = {
+  // Red, Yellow, Green, Pedestrian
+	{0b00001000, 0b00010000, 0b00100000, 0b01000000},  // Port D, E/W
+	{0b00000001, 0b00000010, 0b00000100, 0b00001000}  // Port B, N/S
+};
+
 
 void setTrafficLight(bool northbound, bool toGreen){
 	// TODO: Implement (#2)
@@ -40,5 +46,11 @@ int main(void)
 	DDRB = 0xFF;
 	DDRD = 0b11111011;
 	// TODO: Implement (#5)
+  while (1){
+    for (int i = 0; i<4; i++){
+      PORTD = trafficLights[0][i];
+      _delay_ms(500);
+    }
+  }
 }
 
